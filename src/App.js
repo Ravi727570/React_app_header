bb50e752ea3f23630561386f61cec8b881e1e43c
Ravi727570 <148647125+Ravi727570@users.noreply.github.com>
2025-06-09 14:09:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./header/Header";
+import Summary from "./summary/Summary";
 
 //import CartModal from "./cart/CartModal";
 //import CartContext, { CartProvider } from "./cart/CartContext";
@@ -10,6 +11,7 @@ function App() {
   return (
       <div className="app-container">
         <Header/>
+        <Summary/>
       </div>
     
   );
