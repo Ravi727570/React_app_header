9cfe8115fe9b880ddec987655271c9bddab794e5
Ravi727570 <148647125+Ravi727570@users.noreply.github.com>
2025-06-10 16:38:55 +0530

    Update CartModal.js

--- src/cart/CartModal.js
+++ src/cart/CartModal.js
@@ -1,38 +1,35 @@
-// CartModal.js
 import React from "react";
 import "./CartModal.css";
 
-const CartModal = ({ cartItems, onCloseCart }) => {
+const CartModal = ({ cartItems, onCloseCart, onAddItem, onRemoveItem }) => {
   const totalAmount = cartItems
-    .reduce((sum, item) => sum + item.price * item.amount, 0)
+    .reduce((total, item) => total + item.price * item.amount, 0)
     .toFixed(2);
 
   return (
-    <div className="modal-backdrop">
-      <div className="modal">
-        <h2>Your Cart</h2>
-        <ul className="cart-items">
-          {cartItems.length > 0 ? (
-            cartItems.map((item) => (
-              <li className="cart-item" key={item.id}>
-                <div className="item-details">
-                  <span className="item-name small-text">{item.name}</span>
-                  <span className="item-qty">x {item.amount}</span>
-                </div>
-              </li>
-            ))
-          ) : (
-            <p>Your cart is empty!</p>
-          )}
-        </ul>
-        <div className="cart-total">
-          <h3>Total Amount</h3>
-          <p>{totalAmount}</p>
-        </div>
-        <div className="cart-actions">
-          <button className="close-button" onClick={onCloseCart}>Close</button>
-          <button className="order-button">Order</button>
-        </div>
+    <div className="cart-modal">
+      <ul>
+        {cartItems.map((item) => (
+          <li key={item.id} className="cart-item">
+            <div >
+              <span>{item.name}</span>
+              <span className="icon"> x {item.amount}</span>
+            </div>
+            <div>
+              <span className="clr">${(item.price * item.amount).toFixed(2)}</span>
+              <button className="icon-btn" onClick={() => onRemoveItem(item.id)}>−</button>
+              <button className="icon-btn" onClick={() => onAddItem(item)}>+</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <div className="cart-total">
+        <span>Total Amount</span>
+        <span>${totalAmount}</span>
+      </div>
+      <div className="cart-actions">
+        <button onClick={onCloseCart}>Close</button>
+        <button>Order</button>
       </div>
     </div>
   );
