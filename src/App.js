308efdd68d4b524d4e629b5b4b1bab860d6bc8ab
Ravi727570 <148647125+Ravi727570@users.noreply.github.com>
2025-06-10 11:26:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./header/Header";
 import Summary from "./summary/Summary";
 import Meals from "./meals/Meals";
+import CartModal from "./cart/CartModal";
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
         <main className="content">
           <Summary />
           <Meals />
+          <CartModal/>
         </main>
       </div>
     
