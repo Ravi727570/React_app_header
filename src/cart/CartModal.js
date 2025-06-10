9b23d54911d29f5a29f3384667403c109d059549
Ravi727570 <148647125+Ravi727570@users.noreply.github.com>
2025-06-10 13:35:14 +0530

    Update CartModal.js

--- src/cart/CartModal.js
+++ src/cart/CartModal.js
@@ -1,34 +1,36 @@
+// CartModal.js
 import React from "react";
 import "./CartModal.css";
 
-const CartModal = ({ onCloseCart }) => {
-  const cartItems = [
-    { id: "m1", name: "Sushi", price: 22.99, amount: 1 },
-   
-  ];
-
+const CartModal = ({ cartItems, onCloseCart }) => {
   const totalAmount = cartItems
     .reduce((sum, item) => sum + item.price * item.amount, 0)
     .toFixed(2);
 
-  const productNames = cartItems.map((item) => item.name).join(", ");
-
   return (
-    <div className="backdrop">
+    <div className="modal-backdrop">
       <div className="modal">
-        <div className="cart-header">
-          <span className="cart-product-name">{productNames}</span>
-        </div>
-
+        <h2>Your Cart</h2>
+        <ul className="cart-items">
+          {cartItems.length > 0 ? (
+            cartItems.map((item) => (
+              <li className="cart-item" key={item.id}>
+                <div className="item-details">
+                  <span className="item-name small-text">{item.name}</span>
+                  <span className="item-qty">x {item.amount}</span>
+                </div>
+              </li>
+            ))
+          ) : (
+            <p>Your cart is empty!</p>
+          )}
+        </ul>
         <div className="cart-total">
           <h3>Total Amount</h3>
           <p>{totalAmount}</p>
         </div>
-
         <div className="cart-actions">
-          <button className="close-button" onClick={onCloseCart}>
-            Close
-          </button>
+          <button className="close-button" onClick={onCloseCart}>Close</button>
           <button className="order-button">Order</button>
         </div>
       </div>
